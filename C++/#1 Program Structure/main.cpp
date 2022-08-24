0e8bd7a022cda152f7823b2de7d97ce60c6c9036
bitzs-alex <bitzsalex@gmail.com>
2022-08-24 22:48:44 +0900

    Program Structure session compeleted

---
+++ C++/#1 Program Structure/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+// single line comment
+/*
+two types of comments
+1. single line comment //
+2. mult line comment
+*/
+
+int main() {
+    cout << "Hello World!" << endl;
+    return 0; // functions
+}
