14744647083503fbb81689c7048e421e0d452689
bitzs-alex <bitzsalex@gmail.com>
2022-09-12 09:49:01 +0900

    Variables, Identifiers, and Keywords session updated

--- C++/#3 Variables, Identifiers, and Keywords/main.cpp
+++ C++/#3 Variables, Identifiers, and Keywords/main.cpp
@@ -15,7 +15,7 @@ using namespace std;
 
 /**
  * NOTE:
- * Variables are name of memory areas to hold specific value
+ * Variables are name of memory areas to hold a value
  * 
  * NOTE:
  * CPP is strongly typed programming language, i.e., 
@@ -23,81 +23,95 @@ using namespace std;
  * 
  * NOTE:
  * Identifiers are just names for data items; it could be variable name or class name
- * Identifiers in CPP formed from chars (A-Za-z), digits (0-9), and underscore (_)
- * Identifiers must not start with digits
- * Identifiers in CPP are case sensetive
- * Identifiers must not be preserved keywords
- * Identifiers can range from 1 to 255 characters
- * Associating identifiers with their purpose is a good technique to develop
  */
 
 int main() {
     // strongly typed
     int age;  // age's data type must be specified
 
+
+    // loosely typed and strongly typed programming languages
+    // name = "Abebe";
+    // name = 23;
+    // name = 7.6;
+
+
     // Declaration vs Initialization
     int sum;  // just declaring the variable
     int total = 45;  // initialization, i.e., declaring and assigning initial value
-
+	
+	
     /*
+	
     In the above declarations and initializations, the age, sum, and total are valid identifiers.
-    Invalid identifiers would be:
-        1sum => started with digit
-        and => can not be a reserved keyword
-        total age => contains space
 
-    * @ref for more keywords
-    * https://en.cppreference.com/w/cpp/keyword
+    @ref for more keywords
+    https://en.cppreference.com/w/cpp/keyword
     */
+
 
     // Checking a variable is a memory location
     cout << "total variable : " << total << endl;
     cout << "Getting memory location of total variable : " << &total << endl;
     cout << "Accessing the value stored in the memory location of total variable : " << *(&total) << endl;
+	// We will talk about this concept in detail once we get to Pointers
+	
+	
+	/* 
+	Identifier naming rules
+	=======================
+	Identifiers in CPP formed from chars (A-Za-z), digits (0-9), and underscore (_)
+	Identifiers must not start with digits
+	Identifiers can range from 1 to 255 characters
+	Associating identifiers with their purpose is a good technique to develop
+	*/
+	int age;
+    int age1;
+    int age_1;
+    int _age;
+    int Age;
+	/*
+	The above declarations are completely valid.
+	
+	Invalid identifiers would be:
+        1sum => started with digit
+        long => can not be a reserved keyword
+        total age => contains space
 
-    // other types
-    bool test = false;
-    float salary;
-    char singleCharacter = 'G';  // Notice the single quote here
-    singleCharacter = 'TR';  // Raise a warning but pics the last char
-    // testing
-    cout << singleCharacter << endl;
+    identifier names, can not be keywords
+        int, return, bool, double
+        int double;
+        int long;
+	*/
+	
 
     /*
-    Strings are collection of characters.
-    String is not a built in data type.
-    There are two ways to define strings in CPP */
-    // Method 1: Using character array (more on array on later sessions)
-    char name[12] = "Bitzs Alex";  // Notice the double quote
-    cout << name << endl;
-
-    // Method 2: Using string class
-    string job = "student";  // double quote here as well
-    cout << job << endl;
-
-    // name = "Someone";  // Won't work
-    job = "another thing";
-    cout << job << endl;
-    // More on strings on it's own section
-
-    /*
+	Identifiers in CPP are case sensetive
+	
     Naming Conventions
     Case sensetivity
         sum and Sum are complete different identifiers
     */
-    int twowords;       // flatcase
-    int TWOWORDS;       // UPPERCASE
-    int twoWords;       // camelCase
-    int TwoWords;       // PascalCase
-    int two_words;      // snake_case
-    int TWO_WORDS;      // SCREAMING SNAKE CASE
-    int two_Words;      // camel_Snake_Case
-    int Two_Words;      // Pascal_Snake_Case
+    string twowords;       // flatcase
+    string TWOWORDS;       // UPPERCASE
+    string twoWords;       // camelCase
+    string TwoWords;       // PascalCase
+    string two_words;      // snake_case
+    string TWO_WORDS;      // SCREAMING SNAKE CASE
+    string two_Words;      // camel_Snake_Case
+    string Two_Words;      // Pascal_Snake_Case
     /**
      * @ref for more in naming conventions
      * https://medium.com/wix-engineering/naming-convention-8-basic-rules-for-any-piece-of-code-c4c5f65b0c09
      * https://en.wikipedia.org/wiki/Naming_convention_(programming)#:~:text=In%20computer%20programming,%20a%20naming,in%20source%20code%20and%20documentation.
      */
+
+
+    /**
+     * Declaring multiple variables in a single line
+     * When multiple variables have the same type, we can declare them in one go
+     */
+    int x1, x2, x3, x4;
 
     return 0;
 }
